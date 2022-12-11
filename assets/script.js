0e8930e87f27b4119a9c6219bf021bff492063dd
jwm <jwm3926@gmail.com>
2022-12-10 20:22:10 -0500

    added timer functionality and way to start quiz

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,13 @@
 let questionsArea = document.getElementById('question');
 let answersArea = document.getElementById('answers');
-let optionOne = getElementById('1');
-let optionTwo = getElementById('2');
-let optionThree = getElementById('3');
-let quiz = getElementById('quiz');
+let optionOne = document.getElementById('1');
+let optionTwo = document.getElementById('2');
+let optionThree = document.getElementById('3');
+let quiz = document.getElementById('quiz');
+let startBtn = document.getElementById('start-btn')
+let timeLeft = 75;
+let startScore = 0
+let timer = document.getElementById('timer')
 
 const questions = [
     {
@@ -28,7 +32,54 @@ const questions = [
         correct: 'JavaScript'
     },
     {
-        question: ''
+        question: 'Which of these added style to the webpage?',
+        optionOne: 'MS Paint',
+        optionTwo: 'HTML',
+        optionThree: 'CSS',
+        correct: 'CSS'
     }
-]
+];
+let lastQuestion = questions.length -1;
+let questionIndex = 0;
 
+timer.textContent = "Time Remaining: " + startScore + "s"
+
+startBtn.addEventListener('click', start)
+
+function start() {
+    const timeInterval = setInterval(function() {
+        timer.textContent = "Time remaining: " + timeLeft + 's';
+        timeLeft -=1;
+        if(timeLeft === 0 || questions.length === questionIndex+1){
+            score();
+            clearInterval(timeInterval);
+            timer.textContent = 'Time remaining:' + timeLeft + 's'
+        }
+    }, 1000);
+    loadQuestion();
+};
+
+function loadQuestion() {
+    let question = questions[questionIndex]
+    questionsArea.innerHTML = question.question;
+    optionOne.innerHTML = question.optionOne;
+    optionTwo.innerHTML = question.optionTwo;
+    optionThree.innerHTML = question.optionThree;
+};
+
+function check(answer){
+    if(questions[questionIndex].correct === answer) {
+        answerOutput.textContent = "Correct"
+    } else {
+        answerOutput.textContent = 'incorrect'
+    }
+
+    if (questions.length === questionIndex+1){
+        loadResult();
+        return;
+    }
+    questionIndex++;
+    loadQuestion();
+};
+
+function loadResult
