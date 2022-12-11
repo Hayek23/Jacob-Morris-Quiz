09d4dbb893e09f260d6eb38e0b68c8298691a14e
jwm <jwm3926@gmail.com>
2022-12-10 19:48:48 -0500

    finished HTML and started on JS

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,34 @@
-var time = 75;
-var timerId;
-var timer = document.getElementById("timer")
+let questionsArea = document.getElementById('question');
+let answersArea = document.getElementById('answers');
+let optionOne = getElementById('1');
+let optionTwo = getElementById('2');
+let optionThree = getElementById('3');
+let quiz = getElementById('quiz');
+
+const questions = [
+    {
+        question: 'A boolean is a ______',
+        optionOne: 'A number',
+        optionTwo: 'A sentence',
+        optionThree: 'Something that is either true or false',
+        correct: 'Something that is either true or false'
+    },
+    {
+        question: 'What is the foundation of web development?',
+        optionOne: 'HTML',
+        optionTwo: 'JS',
+        optionThree: 'CSS',
+        correct: 'HTML'
+    },
+    {
+        question: 'What does JS stand for?',
+        optionOne: "Jerry's soup",
+        optionTwo: 'JavaScript',
+        optionThree: 'Jump soon',
+        correct: 'JavaScript'
+    },
+    {
+        question: ''
+    }
+]
+
